69bee1ed89f3a1b25ff8d86b61ec87815161b70b
Joan Marti <joanmartigamez@gmail.com [core]>
2023-10-07 22:12:08 +0200

    Adding Docker Config

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ from typing import List
 
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+import uvicorn
 
 
 app = FastAPI()
@@ -15,6 +15,7 @@ class Person(BaseModel):
     name: str
     age: int
 
+
 DB: List[Person] = [
     Person(id=1, name="Jamila", age=22),
     Person(id=2, name="Alex", age=18),
@@ -22,7 +23,11 @@ DB: List[Person] = [
 
 ]
 
+
 @app.get("/api")
 def read_root():
     return DB
 
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8000, host="127.0.0.1")
